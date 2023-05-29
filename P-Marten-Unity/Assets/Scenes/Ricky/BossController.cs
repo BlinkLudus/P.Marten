086d796a53e7048fe1021979dadb02bbe463c4b2
Ricky Wilkinson <124752575+UnexpectedSensei@users.noreply.github.com>
2023-05-29 14:12:42 +0100

    Small changed made to boss controller code
    small update to boss controller script to make the attack animation happen when player gets within 2 feet

--- P-Marten-Unity/Assets/Scenes/Ricky/BossController.cs
+++ P-Marten-Unity/Assets/Scenes/Ricky/BossController.cs
@@ -6,21 +6,21 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
- //Declaring Variables 
- public Animator bossAnimator;
- public Transform playerTransform;
- public float attackDistance = 1f;
- public string attackTrigger = "Attack";
+    // Declaring Variables
+    public Animator bossAnimator;  // Reference to the Animator component for the boss
+    public Transform playerTransform;  // Reference to the player's Transform component
+    public float attackDistance = 2f;  // The maximum distance at which the boss can attack the player
+    public string attackTrigger = "Attack";  // The trigger parameter name in the boss's animator controller for the attack animation
 
-private void Update()
-{
-    // Measures the distance between the boss and the player
-    float distance = Vector3.Distance(transform.position, playerTransform.position);
+    private void Update()
+    {
+        // Measures the distance between the boss and the player
+        float distance = Vector3.Distance(transform.position, playerTransform.position);
 
-    // If the player is within the attack distance, the boss will initiate his attack
-    if (distance <= attackDistance)
+        // If the player is within the attack distance, the boss will initiate his attack
+        if (distance <= attackDistance)
         {
-            bossAnimator.SetTrigger(attackTrigger);
+            bossAnimator.SetTrigger(attackTrigger);  // Triggers the attack animation in the boss's animator controller
         }
     }
 }
