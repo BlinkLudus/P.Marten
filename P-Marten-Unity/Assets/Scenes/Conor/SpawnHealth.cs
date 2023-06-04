1780f91c769e58a79aa369149c0fa031b1ec642d
BlinkLudus <conor.sheppard@hotmail.com>
2023-06-04 11:12:50 +0100

    health packs now spawn at random locations for the player

---
+++ P-Marten-Unity/Assets/Scenes/Conor/SpawnHealth.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnHealth : MonoBehaviour
+{
+    public GameObject healthPack;
+    public float spawnInterval = 10f; // Time interval between spawns
+    public float spawnRadius = 5f; // Maximum distance from centre 
+
+    private float spawnTimer = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnInterval)
+        {
+            SpawnObject();
+            spawnTimer = 0f;
+        }
+    }
+
+    void SpawnObject()
+    {
+        // Generate a random position within the spawn radius
+        Vector3 randomPosition = Random.insideUnitCircle * spawnRadius;
+        Vector3 spawnPosition = transform.position + randomPosition;
+
+        // Spawn the object at the generated position
+        Instantiate(healthPack, spawnPosition, Quaternion.identity);
+    }
+}
