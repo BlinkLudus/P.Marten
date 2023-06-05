ed7ace9c62db47f9c1f576819a5e4da6a3ab93e5
Cryptic2367 <114927466+Cryptic2367@users.noreply.github.com>
2023-06-05 14:56:57 +0100

    joystick movement complete

--- P-Marten-Unity/Assets/Scenes/Joel/Scripts/PlayerMovement.cs
+++ P-Marten-Unity/Assets/Scenes/Joel/Scripts/PlayerMovement.cs
@@ -5,41 +5,69 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D physicsBody = null;
-    public float speed = 1;
-    public float jumpSpeed = 10;
-    public Collider2D groundSensor = null;
-    public LayerMask groundLayer = 0;
+    private Rigidbody2D physicsBody;
+    public float speed = 1f;
+    public float jumpSpeed = 10f;
+    public float jumpDeadZone = 0.5f;
+    public Collider2D groundSensor;
+    public LayerMask groundLayer;
+    public FixedJoystick joystick;
+    public float jumpCooldown = 1f; // Cooldown time between jumps
+    private float jumpTimer = 0f; // Timer to track jump cooldown
+
     private void Awake()
     {
         physicsBody = GetComponent<Rigidbody2D>();
     }
-public void MoveLeft()
+
+    private void Update()
+    {
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
+
+        HandleMovement(horizontalInput);
+        HandleJump(verticalInput);
+    }
+
+    private void HandleMovement(float horizontalInput)
+    {
+        if (horizontalInput < 0 || horizontalInput > 0)
+        {
+            Move(horizontalInput);
+        }
+    }
+
+    private void HandleJump(float verticalInput)
+    {
+        if (verticalInput > jumpDeadZone && groundSensor.IsTouchingLayers(groundLayer) && jumpTimer <= 0f)
+        {
+            Jump();
+            jumpTimer = jumpCooldown;
+        }
+
+        // Update the jump timer
+        if (jumpTimer > 0f)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Move(float horizontalInput)
     {
         Vector2 newVelocity = physicsBody.velocity;
-
-        newVelocity.x = -speed;
-
+        newVelocity.x = horizontalInput * speed;
         physicsBody.velocity = newVelocity;
     }
 
-public void MoveRight()
-{
-    Vector2 newVelocity = physicsBody.velocity;
-
-    newVelocity.x = speed;
-
-    physicsBody.velocity = newVelocity;
-}
-    public void Jump()
+    private void Jump()
     {
-        if (groundSensor.IsTouchingLayers(groundLayer))
-        {
-            Vector2 newVelocity = physicsBody.velocity;
-
-            newVelocity.y = jumpSpeed;
-
-            physicsBody.velocity = newVelocity;
-        }
+        Vector2 newVelocity = physicsBody.velocity;
+        newVelocity.y = jumpSpeed;
+        physicsBody.velocity = newVelocity;
     }
-    }
+}
+
+
+
+
+
